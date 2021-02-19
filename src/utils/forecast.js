b3ee90915f52cd901060eb33708d745b95921d18
Antonio Klauda <jojobj@windowslive.com>
2021-02-19 18:44:33 +0100

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('postman-request')
+
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=ce98803d1c2a0eaa98e6bf76f57453ec&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&units=m'
+    
+
+    request({ url, json: true }, (error, { body }) => {
+        if(error){
+            callback('Unable to connect to weather service!', undefined)
+        }
+        else if(body.error){
+            callback('Unable to find location!', undefined)
+        }
+        else{ 
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' celsius out. It feels like ' + body.current.feelslike + ' celsius.')
+        }
+    })
+}
+
+module.exports = forecast
